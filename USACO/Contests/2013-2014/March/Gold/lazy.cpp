e49a79fced7e5e84e81716fc1bedc9ee8cb7010b
WhaleVomit <ldorianchen@gmail.com>
2017-10-18 17:01:36 -0400

    Update lazy.cpp

--- USACO/Contests/2013-2014/March/Gold/lazy.cpp
+++ USACO/Contests/2013-2014/March/Gold/lazy.cpp
@@ -32,55 +32,88 @@ typedef vector<int> vi;
 typedef pair<int,int> pi;
 
 pair<pi, int> patches[MAX_N];
-map<int, int> cx;
-map<int, int> cy;
 
 int n, k;
 pair<pi, pi> sp[MAX_N];
 pair<pi, pi> ep[MAX_N];
 
-template<int SZ> struct lazymaxtree {
-    int mx[2*SZ], lazy[2*SZ]; // set SZ to a power of 2
-    
-    lazymaxtree() {
-        F0R(i, 2*SZ) mx[i] = 0, lazy[i] = 0;
+struct node {
+    int val;
+    int lazy;
+    int l, r;
+    node* left;
+    node* right;
+    node(int l, int r) {
+        this -> val = 0;
+        this -> lazy = 0;
+        this -> l = l;
+        this -> r = r;
+        this -> left = nullptr;
+        this -> right = nullptr;
     }
-    
-    void push(int ind, int L, int R) {
-        mx[ind] += lazy[ind];
-        if (L != R) lazy[2*ind] += lazy[ind], lazy[2*ind+1] += lazy[ind];
-        lazy[ind] = 0;
-    }
-    
-    void pull(int ind) {
-        mx[ind] = max(mx[2*ind],mx[2*ind+1]);
-    }
-    
-    void build() {
-        for (int i = SZ-1; i > 0; --i) pull(i);
-    }
+};
 
-    int query(int lo, int hi, int ind = 1, int L = 0, int R = SZ-1) {
-        push(ind,L,R);
-        if (lo > R || L > hi) return -1000000000;
-        if (lo <= L && R <= hi) return mx[ind];
-        
-        int M = (L+R)/2;
-        return max(query(lo,hi,2*ind,L,M),query(lo,hi,2*ind+1,M+1,R));
+template<int SZ> struct lazymaxtree {
+    node* root;
+    lazymaxtree() {
+        int ub = 1;
+        while(ub < SZ) ub *= 2;
+        root = new node(0, ub-1);
     }
-    
-    void upd(int lo, int hi, int inc, int ind = 1, int L = 0, int R = SZ-1) {
-        push(ind,L,R);
-        if (hi < L || R < lo) return;
-        if (lo <= L && R <= hi) {
-            lazy[ind] = inc;
-            push(ind,L,R);
+    void propagate(node* n) {
+        if(n->l != n->r) {
+            int mid = ((n->l) + (n->r))/2;
+            if(n->left == nullptr) {
+                n->left = new node(n->l, mid);
+            }
+            if(n->right == nullptr) {
+                n->right = new node(mid+1, n->r);
+            }
+        }
+        if(n->lazy != 0) {
+            n->val += n->lazy;
+            if(n->l != n->r) {
+                n->left->lazy += n->lazy;
+                n->right->lazy += n->lazy;
+            }
+            n->lazy = 0;
+        }
+    }
+    void updN(node* n, int i1, int i2, int val) {
+        propagate(n);
+        if(i2 < n->l || i1 > n->r) return;
+        if(n->l == n->r) {
+            n->val += val;
+            return;
+        }
+        if(i1 <= n->l && i2 >= n->r) {
+            n->val += val;
+            n->left->lazy += val;
+            n->right->lazy += val;
             return;
         }
         
-        int M = (L+R)/2;
-        upd(lo,hi,inc,2*ind,L,M); upd(lo,hi,inc,2*ind+1,M+1,R);
-        pull(ind);
+        updN(n->left, i1, i2, val);
+        updN(n->right, i1, i2, val);
+        n->val = max(n->left->val, n->right->val);
+    }
+    void upd(int i1, int i2, int val) {
+        updN(root, i1, i2, val);
+    }
+    int queryN(node* n, int i1, int i2) {
+        propagate(n);
+        if(i2 < n->l || i1 > n->r) return -(1<<30)+1-(1<<30);
+        if(n->l == n->r) {
+            return n->val;
+        }
+        if(i1 <= n->l && i2 >= n->r) {
+            return n->val;
+        }
+        
+        return max(queryN(n->left, i1, i2), queryN(n->right, i1, i2));
+    }
+    int query(int i1, int i2) {
+        return queryN(root, i1, i2);
     }
 };
 
@@ -95,33 +128,21 @@ int main() {
         patches[i].F.S = y-x;
     }
     F0R(i, n) {
-        sp[i].F.F = patches[i].F.F - k; cx[sp[i].F.F] = 0;
-        sp[i].S.F = patches[i].F.S - k; cy[sp[i].S.F] = 0;
-        sp[i].S.S = patches[i].F.S + k; cy[sp[i].S.S] = 0;
+        sp[i].F.F = 3000000+patches[i].F.F - k;
+        sp[i].S.F = 3000000+patches[i].F.S - k;
+        sp[i].S.S = 3000000+patches[i].F.S + k;
         
-        ep[i].F.F = patches[i].F.F + k; cx[ep[i].F.F] = 0;
-        ep[i].S.F = patches[i].F.S - k; cy[ep[i].S.F] = 0;
-        ep[i].S.S = patches[i].F.S + k; cy[ep[i].S.S] = 0;
+        ep[i].F.F = 3000000+patches[i].F.F + k;
+        ep[i].S.F = 3000000+patches[i].F.S - k;
+        ep[i].S.S = 3000000+patches[i].F.S + k;
         
         sp[i].F.S = patches[i].S;
         ep[i].F.S = -patches[i].S;
     }
-    int k1 = 0, k2 = 0;
-    for(auto& v : cx) v.S = k1++;
-    for(auto& v : cy) v.S = k2++;
-    F0R(i, n) {
-        sp[i].F.F = cx[sp[i].F.F];
-        sp[i].S.F = cy[sp[i].S.F];
-        sp[i].S.S = cy[sp[i].S.S];
-        
-        ep[i].F.F = cx[ep[i].F.F];
-        ep[i].S.F = cy[ep[i].S.F];
-        ep[i].S.S = cy[ep[i].S.S];
-    }
     sort(begin(sp), begin(sp) + n);
     sort(begin(ep), begin(ep) + n);
     
-    lazymaxtree<1<<18> tree;
+    lazymaxtree<5000000> tree;
     int indS = 0;
     int indE = 0;
     int hi = 0;
@@ -130,7 +151,7 @@ int main() {
             int xval = sp[indS].F.F;
             while(indS < n && sp[indS].F.F == xval)
                 tree.upd(sp[indS].S.F, sp[indS].S.S, sp[indS].F.S), indS++;
-            hi = max(hi, tree.query(0, 2*n-1));
+            hi = max(hi, tree.query(0, 5000000-1));
         }
         else {
             int xval = ep[indE].F.F;
