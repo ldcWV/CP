cc9e81b88d85e67cf2278e774e6a9879c59affc2
WhaleVomit <ldorianchen@gmail.com>
2017-11-14 17:33:38 -0500

    Add files via upload

--- USACO/Contests/2013-2014/January/Gold/skicourse.cpp
+++ USACO/Contests/2013-2014/January/Gold/skicourse.cpp
@@ -1,9 +1,3 @@
-/*
- ID: ldorian1
- LANG: C++11
- TASK: skicourse
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,6 +10,7 @@
 #include <set>
 #include <stack>
 #include <map>
+#include <assert.h>
 
 #define F first
 #define S second
@@ -24,80 +19,73 @@
 #define SQ(a) (a)*(a)
 #define PMOD(a, b) (((a)+(b))%(b))
 
-#define FOR(i, a, b) for (int i=a; i<b; i++)
-#define F0R(i, a) for (int i=0; i<a; i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define MOO(i, a, b) for (int i=a; i<b; i++)
+#define M00(i, a) for (int i=0; i<a; i++)
+#define MOOd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define M00d(i,a) for (int i = (a)-1; i >= 0; i--)
 
-#define MAX_N 100
+#define FAST ios::sync_with_stdio(0); cin.tie(0);
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> pi;
+typedef pair<int,int> pii;
 
-int n, m;
+const int MAX_N = 100;
 int arr[MAX_N][MAX_N];
-int dp1[MAX_N][MAX_N];
-int dp2[MAX_N][MAX_N];
+int dp1[MAX_N][MAX_N]; //max size without 1
+int dp2[MAX_N][MAX_N]; //max size without 2
+int n, m;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() { FAST
     ofstream fout("skicourse.out");
     ifstream fin("skicourse.in");
+    int lo = 500;
     fin >> n >> m;
-    F0R(i, n) F0R(j, m) {
-        char s; fin >> s;
-        if(s=='R') arr[i][j]=1;
-        else arr[i][j]=2;
+    M00(i, n) M00(j, m) {
+        char c; fin >> c;
+        arr[i][j] = (c=='R') ? 1:2;
     }
-    int ans = 100;
-    while(true) {
-        int sz = 0;
-        F0R(i, n) {
-            dp1[i][0] = (arr[i][0]==0 || arr[i][0]==1);
-            dp2[i][0] = (arr[i][0]==0 || arr[i][0]==2);
+    M00(i, n*m) {
+        M00(i, n) {
+            dp1[i][m-1] = (arr[i][m-1] == 2 || arr[i][m-1] == 0) ? 1 : 0;
+            dp2[i][m-1] = (arr[i][m-1] == 1 || arr[i][m-1] == 0) ? 1 : 0;
         }
-        F0R(i, m) {
-            dp1[0][i] = (arr[0][i]==0 || arr[0][i]==1);
-            dp2[0][i] = (arr[0][i]==0 || arr[0][i]==2);
+        M00(i, m) {
+            dp1[n-1][i] = (arr[n-1][i] == 2 || arr[n-1][i] == 0) ? 1 : 0;
+            dp2[n-1][i] = (arr[n-1][i] == 1 || arr[n-1][i] == 0) ? 1 : 0;
         }
-        FOR(i, 1, n) FOR(j, 1, m) {
-            if(arr[i][j]==1) {
-                dp1[i][j] = 1+min(dp1[i][j-1], min(dp1[i-1][j], dp1[i-1][j-1]));
-                dp2[i][j] = 0;
-            }
-            else if(arr[i][j]==2) {
+        M00d(i, n-1) M00d(j, m-1) {
+            if(arr[i][j] == 1) {
                 dp1[i][j] = 0;
-                dp2[i][j] = 1+min(dp2[i][j-1], min(dp2[i-1][j], dp2[i-1][j-1]));
-            }
-            else {
-                dp1[i][j] = 1+min(dp1[i][j-1], min(dp1[i-1][j], dp1[i-1][j-1]));
-                dp2[i][j] = 1+min(dp2[i][j-1], min(dp2[i-1][j], dp2[i-1][j-1]));
+            } else {
+                dp1[i][j] = 1 + min(min(dp1[i+1][j], dp1[i][j+1]), dp1[i+1][j+1]);
             }
         }
-        F0R(i, n) F0R(j, m) {
-            if(!(dp1[i][j] == dp2[i][j])) {
-                sz = max(sz, max(dp1[i][j], dp2[i][j]));
+        M00d(i, n-1) M00d(j, m-1) {
+            if(arr[i][j] == 2) {
+                dp2[i][j] = 0;
+            } else {
+                dp2[i][j] = 1 + min(min(dp2[i+1][j], dp2[i][j+1]), dp2[i+1][j+1]);
             }
         }
-        bool done = true;
-        F0R(i, n) F0R(j, m) if(arr[i][j] != 0) done = false;
-        if(done) break;
-        ans = min(ans, sz);
-        F0R(i, n) F0R(j, m) if(!(dp1[i][j] == dp2[i][j])){
-            if((dp1[i][j] == sz) || (dp2[i][j] == sz)) {
-                FOR(a, i-sz+1, i+1) {
-                    FOR(b, j-sz+1, j+1) {
-                        arr[a][b] = 0;
-                    }
-                }
-                goto out;
+        int hi = 0;
+        int bi = 0;
+        int bj = 0;
+        M00(i, n) M00(j, m) if(dp1[i][j] != dp2[i][j]) {
+            int b = max(dp1[i][j], dp2[i][j]);
+            if(b > hi) {
+                hi = max(hi, b);
+                bi = i;
+                bj = j;
             }
         }
-        out: ;
+        MOO(i, bi, bi+hi) MOO(j, bj, bj+hi) arr[i][j] = 0;
+        lo = min(lo, hi);
+        bool out = 1;
+        M00(i, n) M00(j, m) if(arr[i][j] != 0) out = 0;
+        if(out) break;
     }
-    fout << ans << "\n";
+    fout << lo << "\n";
 }
