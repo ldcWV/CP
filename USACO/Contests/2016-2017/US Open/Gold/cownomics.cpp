7bac9de49a32576319990ec5d465f9ccd8fa7447
WhaleVomit <ldorianchen@gmail.com>
2017-12-21 15:52:51 -0500

    Add files via upload

--- USACO/Contests/2016-2017/US Open/Gold/cownomics.cpp
+++ USACO/Contests/2016-2017/US Open/Gold/cownomics.cpp
@@ -1,9 +1,3 @@
-/*
- ID: ldorian1
- LANG: C++11
- TASK: cownomics
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -15,40 +9,95 @@
 #include <queue>
 #include <set>
 #include <stack>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <assert.h>
 
-#define FOR(i, a, b) for (int i=a; i<b; i++)
-#define F0R(i, a) for (int i=0; i<a; i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define MAX_N 500
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define SQ(a) (a)*(a)
+
+#define MOO(i, a, b) for (int i=a; i<b; i++)
+#define M00(i, a) for (int i=0; i<a; i++)
+#define MOOd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define M00d(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0);
 
 using namespace std;
 
-string spotty[MAX_N];
-string normal[MAX_N];
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+const ll MOD = 1000000007;
+const int MAX_N = 500;
+
 int n, length;
 
-bool works(int seqLength) {
-    F0R(i, length-seqLength+1) {
-        set<string> s1;
-        bool w = true;
-        F0R(j, n) s1.insert(spotty[j].substr(i, seqLength));
-        F0R(j, n) if(s1.count(normal[j].substr(i, seqLength))) w = false;
-        if(w) return true;
+template<int SZ> struct rabinKarp {
+    const ll mods[3] = {1000000007, 999119999, 1000992299};
+    ll p[3][SZ];
+    ll h[3][SZ];
+    const ll base = 1000696969;
+    rabinKarp() {}
+    void build(string a) {
+        M00(i, 3) {
+            p[i][0] = 1;
+            h[i][0] = (int)a[0];
+            MOO(j, 1, (int)a.length()) {
+                p[i][j] = (p[i][j-1] * mods[i]) % base;
+                h[i][j] = (h[i][j-1] * mods[i] + (int)a[j]) % base;
+            }
+        }
     }
-    return false;
+    tuple<ll, ll, ll> hsh(int a, int b) {
+        if(a == 0) return make_tuple(h[0][b], h[1][b], h[2][b]);
+        tuple<ll, ll, ll> ans;
+        get<0>(ans) = (((h[0][b] - h[0][a-1]*p[0][b-a+1]) % base) + base) % base;
+        get<1>(ans) = (((h[1][b] - h[1][a-1]*p[1][b-a+1]) % base) + base) % base;
+        get<2>(ans) = (((h[2][b] - h[2][a-1]*p[2][b-a+1]) % base) + base) % base;
+        return ans;
+    }
+};
+
+ll toLL(tuple<ll, ll, ll> a) {
+    return get<0>(a);
 }
 
-int main()
-{
+rabinKarp<MAX_N> spotty[MAX_N];
+rabinKarp<MAX_N> normal[MAX_N];
+
+bool works(int len) {
+    M00(i, length - len + 1) {
+        bool w = 1;
+        unordered_set<ll> asdf;
+        M00(j, n) asdf.insert(toLL(spotty[j].hsh(i, i+len-1)));
+        M00(j, n) if(asdf.count(toLL(normal[j].hsh(i, i+len-1)))) w = 0;
+        if(w) return 1;
+    }
+    return 0;
+}
+
+int main() {
     ofstream fout("cownomics.out");
     ifstream fin("cownomics.in");
     fin >> n >> length;
-    F0R(i, n) fin >> spotty[i];
-    F0R(i, n) fin >> normal[i];
-    int d = 0;
-    for(int jump = length/2; jump>=1; jump/=2) {
-        while(d+jump<=length && !works(d+jump)) d+=jump;
+    M00(i, n) {
+        string a; fin >> a;
+        spotty[i].build(a);
     }
-    fout << d+1 << "\n";
+    M00(i, n) {
+        string a; fin >> a;
+        normal[i].build(a);
+    }
+    int currLen = 1;
+    for(int jump = length; jump > 0; jump /= 2) {
+        while(currLen + jump <= length && !works(currLen+jump)) currLen += jump;
+    }
+    fout << currLen + 1 << "\n";
 }
