45c14616e6e9a35dc24afccd6cc693ab65848b0b
Lawrence <Ldorianchen@gmail.com>
2020-05-29 11:11:52 -0400

    reorganize

--- Battlecode/2019/Prophet.java
+++
@@ -1,8 +0,0 @@
-package bc19;
-
-import static bc19.Consts.*;
-
-public class Prophet extends Attackable {
-    public Prophet(MyRobot z) { super(z); }
-    Action2 run() { return runDefault(); }
-}
